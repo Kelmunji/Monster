2a3d86a45d2da1287d38c1924a0493e547b8dd32
kelvin Githinji <githinjikelvin74@gmail.com>
2025-06-20 09:52:42 +0300

    initial changes

--- db/database.py
+++
@@ -1,8 +0,0 @@
-# db/database.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "sqlite:///monsters.db"
-
-engine = create_engine(DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- db/session.py
+++
@@ -1,5 +0,0 @@
-# db/session.py
-from db.database import SessionLocal
-
-# Create a session
-session = SessionLocal()
